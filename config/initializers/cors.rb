3e59036d8ae0bee19d1c445e1d8977dc145a5701
Juan Henao <juanhenao@Juans-MacBook-Pro.local>
2024-01-31 09:29:01 -0500

    configuration devise and controllers registrations and sessions

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
     resource '*',
     headers: :any,
-    methods: [:get, :post, :patch, :put]
+    methods: [:get, :post, :patch, :put],
+    expose: ["Authorization"]
   end
 end
