1b8ec3ebe3a283abec5ff41fc5c707140e779c5d
Juan Henao <juanhenao@Juans-MacBook-Pro.local>
2024-02-03 19:13:25 -0500

    adding name in user, adding can manage message no authenticate user for send and view messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :messages_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
     render json: { message: exception, status: 'authorization_failed'}
+  end
+
+  private
+
+  def messages_controller?
+    controller_name == 'messages'
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
+    # Permiso para cualquier usuario, incluso no autenticado, para crear mensajes
+    can :manage, Message
     if user.admin?
       can :manage, :all
     end
